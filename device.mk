bb3773cc5b4ddb5b8ec57df619521ac68f13d2bf
hridaya <hridaya@pixelos.net>
2026-03-18 18:03:41 +0545

    Revert "gauguin: Drop libcameraservice extension"
    This reverts commit 527149521e8075bcdcf06e8acdacb0c68b7b9516.
    
    Change-Id: I0ec0b70b2cbda0e7c0865ac579019065ff5110f5

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_gauguin)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service.lineage
