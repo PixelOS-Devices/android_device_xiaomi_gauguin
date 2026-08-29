92e4757a6cefdbf238fc49711c10f91b9a2337c4
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    gauguin: Drop UFFD flag
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- device.mk
+++ device.mk
@@ -251,9 +251,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/usr/keylayout/,$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout)
