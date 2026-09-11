8372d4fe6f62c7c49490cf6ebc0ca8a96c0fb043
hridaya <hridaya@pixelos.net>
2026-09-11 11:03:23 +0545

    Revert "gauguin: Setup prebuilt a-HAL"
    We're going to be using OSS a-HAL now.
    
    This reverts commit 1570eaa0bfecca3b91212e37c3e29ac58b88d5d8.
    
    Change-Id: Ia7057a8c778f7cd09c11655cf8418e8174374025

--- device.mk
+++ device.mk
@@ -90,9 +90,18 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.lito
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libbatterylistener \
+    libcomprcapture \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
+    libtinycompress \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
